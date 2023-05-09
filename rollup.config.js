2aa45a3d0b84ef39611f1564cd400d90dc8b561f
Gustavo Neves <gagneves@hotmail.com>
2023-05-09 19:55:42 -0400

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import postcss from "rollup-plugin-postcss";
+import dts from 'rollup-plugin-dts';
+
+const pkg = require('./package.json');
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourceMap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourceMap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss({ modules: true })
+    ],
+  },
+  {
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
+];
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React, { InputHTMLAttributes } from 'react';
+import styles from './styles.module.css'
+
+export interface OwnProps extends InputHTMLAttributes<HTMLButtonElement> {
+  label: string;
+}
+
+const ButtonWrapper = (props: OwnProps) => {
+  return (
+    <div>
+      <button className={styles.button}>{props.label}</button>
+    </div>
+  );
+};
+
+export default ButtonWrapper;
